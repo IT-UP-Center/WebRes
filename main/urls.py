dd13868619f9003812bc7c5b1388002de5040e3e
Wax30d <shaxzodbek_sobitov@mail.ru>
2025-07-11 17:34:31 +0500

    Added about views

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hello_view, name='about'),
-
+    path('', views.hello_view, name='main'),
+    path('about/', views.about_view, name='about'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,10 @@
 
 from django.http import HttpResponse
 
+
 def hello_view(request):
     return HttpResponse("Привет, мир!")
 
 
-
+def about_view(request):
+    return HttpResponse("About")
